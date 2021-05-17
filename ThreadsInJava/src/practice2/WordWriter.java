ce86819df2d27b3254473fb421dbb57ca511bb1c
aymansalem22 <s.aymansalem@gmail.com>
2021-05-17 16:58:29 +0200

    flush and how write in two files

---
+++ ThreadsInJava/src/practice2/WordWriter.java
@@ -0,0 +1,29 @@
+package practice2;
+
+import java.io.PrintWriter;
+
+public class WordWriter implements Runnable {
+
+    PrintWriter out;
+
+    public WordWriter(PrintWriter out) {
+        this.out = out;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            out.println("first");
+            out.println("second");
+            out.println("third");
+            out.println("fourth");
+            out.println("fifth");
+            out.flush();
+
+        } catch (Exception e) {
+        }
+    }
+
+}
